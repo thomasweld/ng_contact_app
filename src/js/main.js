a8c11eafba695602a38848197cd697fc88af9e1f
thomasweld <thomas.weld@gmail.com>
2016-05-20 22:45:58 -0400

    angular working

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,18 @@ import { SinglePeepController } from './controllers/singlePeepController';
 import { AddPeepController } from './controllers/addPeepController';
 
 // Import config file
-import { config } from './config';
+// import { config } from './config';
+
+// Start Angular / declare a module for myApp
+angular
+  // Setter for a module (register)
+  .module('myApp', ['ui.router'])
+  // Register Our Configuration
+  // .config(config)
+  // // App URL Constant
+  // .constant('URL', url)
+  // // Register Our Controllers
+  // .controller('ListController', ListController)
+  // .controller('SingleController', SingleController)
+  // .controller('AddController', AddController)
+;
